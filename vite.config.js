32b2c3029fe598aee675334f685b71a3aa6c7576
P JESHWANTH SAI <163727911+KLU2300030520@users.noreply.github.com>
2025-09-24 09:45:17 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/adoptly/',   // important for Tomcat
+  base: '/Adoptly/',   // important for Tomcat
 })
